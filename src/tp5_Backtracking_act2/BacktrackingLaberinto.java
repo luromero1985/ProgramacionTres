a8293f1b96033975e89d530ca24798369f30cb6c
Luciana Yael Romero <lu.yael.r@gmail.com>
2025-11-29 13:13:32 -0300

    inclui las consignas en act 1 y 2 de tp5

--- src/tp5_Backtracking_act2/BacktrackingLaberinto.java
+++ src/tp5_Backtracking_act2/BacktrackingLaberinto.java
@@ -3,6 +3,16 @@ package tp5_Backtracking_act2;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Ejercicio 2
+Dado un laberinto consistente en una matriz cuadrada que tiene en cada posición un valor natural y
+cuatro valores booleanos, indicando estos últimos si desde esa casilla se puede ir al norte, este, sur
+y oeste, encontrar un camino de longitud mínima entre dos casillas dadas, siendo la longitud de un
+camino la suma de los valores naturales de las casillas por las que pasa. Idea: podría representarse
+el laberinto como una matriz, de objetos, donde cada objeto tiene el valor natural, y cuatro
+booleanos, uno para cada dirección a la que se permite ir desde allí.
+ * */
+
 public class BacktrackingLaberinto {
 	private Laberinto lab;
     private boolean[][] visitado;
--- src/tp5_backtracking_act1/BacktrackingSalas.java
+++ src/tp5_backtracking_act1/BacktrackingSalas.java
@@ -5,7 +5,15 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 import tp4_grafos.GrafoDirigido;
+/*
+ * Ejercicio 1
+Se tiene un conjunto de salas comunicadas entre sí a través de puertas que se abren solamente en
+un sentido. Una de las salas se denomina entrada y la otra salida. Construir un algoritmo que
+permita ir desde la entrada a la salida atravesando la máxima cantidad de salas. Idea: podría
+representar el problema mediante un grafo dirigido, donde cada nodo es una habitación, y cada
+puerta es un arco dirigido hacia otra habitación.
 
+ * */
 public class BacktrackingSalas {
 
     private ArrayList<Integer> visitadas;
